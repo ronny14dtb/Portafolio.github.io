b51720fa0326b05db666629b1d9326dfa9308244
ronny14dtb <ronnydario8@outlook.com>
2024-12-19 16:33:42 -0500

    Se actualiza el codigo del slider pero queda con un espacio en el botón de anterior por un margin automático y la imagen dentro del slider va sin border

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Carta con corazones
+
 let next = document.querySelector('.next')
 let prev = document.querySelector('.prev')
 
@@ -38,9 +40,10 @@ $( document ).ready(function() {
    
 });
 
+// botones de avanzar y retroceder
 
 function Siguiente(){
-    document.getElementById("carrousel_main").style.display = "block";
+    document.getElementById("parte1").style.display = "block";
     document.getElementById("anterior").style.display = "block";
 
     document.getElementById("carta").style.display = "none";
@@ -50,7 +53,7 @@ function Siguiente(){
 }
 
 function Anterior(){
-    document.getElementById("carrousel_main").style.display = "none";
+    document.getElementById("parte1").style.display = "none";
     document.getElementById("anterior").style.display = "none";
 
     document.getElementById("carta").style.display = "block";
@@ -58,3 +61,69 @@ function Anterior(){
 
 
 }
+
+
+//Nuevo carrousel 
+
+
+var nextBtn = document.querySelector('.next'),
+    prevBtn = document.querySelector('.prev'),
+    carousel = document.querySelector('.carousel'),
+    list = document.querySelector('.list'), 
+    item = document.querySelectorAll('.item'),
+    runningTime = document.querySelector('.carousel .timeRunning') 
+
+let timeRunning = 3000 
+let timeAutoNext = 7000
+
+nextBtn.onclick = function(){
+    showSlider('next')
+}
+
+prevBtn.onclick = function(){
+    showSlider('prev')
+}
+
+let runTimeOut 
+
+let runNextAuto = setTimeout(() => {
+    nextBtn.click()
+}, timeAutoNext)
+
+
+function resetTimeAnimation() {
+    runningTime.style.animation = 'none'
+    runningTime.offsetHeight /* trigger reflow */
+    runningTime.style.animation = null 
+    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
+}
+
+
+function showSlider(type) {
+    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
+    if(type === 'next'){
+        list.appendChild(sliderItemsDom[0])
+        carousel.classList.add('next')
+    } else{
+        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
+        carousel.classList.add('prev')
+    }
+
+    clearTimeout(runTimeOut)
+
+    runTimeOut = setTimeout( () => {
+        carousel.classList.remove('next')
+        carousel.classList.remove('prev')
+    }, timeRunning)
+
+
+    clearTimeout(runNextAuto)
+    runNextAuto = setTimeout(() => {
+        nextBtn.click()
+    }, timeAutoNext)
+
+    resetTimeAnimation() // Reset the running time animation
+}
+
+// Start the initial animation 
+resetTimeAnimation()
